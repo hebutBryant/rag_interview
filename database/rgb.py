f2b348c7f3264f91911faa43a88994f1db37a91b
hebutBryant <1565561624@qq.com>
2026-04-08 16:14:53 +0800

    add rgb

---
+++ database/rgb.py
@@ -0,0 +1,89 @@
+import json
+import os
+from typing import List
+from utils.base import read_json
+
+RGB_DATAPATH = "/home/hdd/depcache/dataset/RGB"
+RGB_KB_DATAPATH = "/home/hdd/depcache/dataset/RGB/rgb_triplets.json"
+
+
+from utils.base import exists
+
+
+
+def concat_strings_in_list(input_list):
+    if not isinstance(input_list, list):
+        raise ValueError("The input must be a list.")
+
+    if isinstance(input_list[0], list):
+        input_list = [" ".join(sublist) for sublist in input_list]
+
+    assert all(isinstance(item, str) for item in input_list)
+    return input_list
+
+
+def get_rgb_info(file="en", chunk_size=512):
+    data_file = os.path.join(RGB_DATAPATH, f"{file}.json")
+    assert exists(data_file), f"{data_file} not exist!"
+
+    texts = []
+    questions = []
+    answers = []
+    with open(data_file, "r") as f:
+        for line in f:
+            instance = json.loads(line)
+            if file == "en_fact":
+                pos_text = " ".join(concat_strings_in_list(instance["positive"]))
+                neg_text = " ".join(concat_strings_in_list(instance["negative"]))
+                texts.append(pos_text + "\n" + neg_text)
+            elif file == "en_int":
+                pos_texts = concat_strings_in_list(instance["positive"])
+                neg_texts = concat_strings_in_list(instance["negative"])
+                # texts.append(compact_string(pos_texts, chunk_size=chunk_size))
+                texts.append(
+                    compact_string(pos_texts + neg_texts, chunk_size=chunk_size)
+                )
+                # print(len(texts[-1]))
+
+            else:
+                texts += concat_strings_in_list(instance["positive"])
+                texts += concat_strings_in_list(instance["negative"])
+            questions.append(instance["query"])
+            answers.append(instance["answer"])
+
+    # all_len = [len(x) for x in texts]
+    # print(all_len[:5])
+    # print(sum(all_len))
+
+    # alpha_count = sum([len(x) for x in texts])
+    # word_count = sum([len(x.split(" ")) for x in texts])
+    # print(f"texts: {len(texts)}, alpha: {alpha_count}, word_count: {word_count}")
+
+    data_info = {
+        "texts": texts,
+        "questions": questions,
+        "answers": answers,
+    }
+
+    return data_info
+
+def get_triplets():
+    triplets = read_json(RGB_KB_DATAPATH)
+    triplets = list(set(tuple(t) for t in triplets))
+    return triplets
+
+if __name__ == "__main__":
+
+    # choices=["en", "zh", "en_int", "zh_int", "en_fact", "zh_fact"],
+
+    rgb_info = get_rgb_info("en")
+    print(len(rgb_info["texts"]), len(rgb_info["questions"]), len(rgb_info["answers"]))
+
+    rgb_info = get_rgb_info("en_int")
+    print(len(rgb_info["texts"]), len(rgb_info["questions"]), len(rgb_info["answers"]))
+
+    rgb_info = get_rgb_info("en_fact")
+    print(len(rgb_info["texts"]), len(rgb_info["questions"]), len(rgb_info["answers"]))
+
+    rgb_info = get_rgb_info("en_refine")
+    print(len(rgb_info["texts"]), len(rgb_info["questions"]), len(rgb_info["answers"]))
